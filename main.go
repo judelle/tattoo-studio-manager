187b2c757567ab42bcc63505b7e866351953af99
Judelle <sasha_lumpov@mail.ru>
2026-09-24 23:24:22 +0300

    add validation

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"os"
 	"slices"
-	"strconv"
 	"strings"
+
+	"github.com/google/uuid"
 )
 
 type Appointment struct {
-	ID           int
-	ClientID     int
+	ID           string
+	ClientID     string
 	Date         string
 	Time         string
 	Description  string
@@ -23,7 +24,7 @@ type Appointment struct {
 }
 
 type Client struct {
-	ID      int
+	ID      string
 	Name    string
 	IsAdult bool
 	Phone   string
@@ -48,7 +49,7 @@ func main() {
 		fmt.Println("-------------------")
 		switch choice {
 		case "1":
-			newClient := addClient(scanner, len(clients)+1)
+			newClient := addClient(scanner, clients)
 			clients = append(clients, newClient)
 		case "2":
 			printClients(clients)
@@ -82,50 +83,86 @@ func printClient(client Client) {
 	fmt.Println("-------------------")
 }
 
-func findClientByID(clients []Client, id int) (Client, error) {
+func findClientByID(clients []Client, id string) (Client, error) {
 	for _, client := range clients {
 		if client.ID == id {
 			return client, nil
 		}
 	}
 
-	return Client{}, fmt.Errorf("клиент с ID %d не найден", id)
+	return Client{}, fmt.Errorf("клиент с ID %s не найден", id)
 }
 
-func findClientIndexByID(clients []Client, id int) (int, error) {
+func findClientIndexByID(clients []Client, id string) (int, error) {
 	for i, client := range clients {
 		if client.ID == id {
 			return i, nil
 		}
 	}
 
-	return -1, fmt.Errorf("клиент с ID %d не найден", id)
+	return -1, fmt.Errorf("клиент с ID %s не найден", id)
 }
 
-func addClient(scanner *bufio.Scanner, id int) Client {
-
-	fmt.Print("Введите имя: ")
-	scanner.Scan()
-	name := strings.TrimSpace(scanner.Text())
-
-	fmt.Print("Клиент совершеннолетний? Д/Н: ")
-	scanner.Scan()
-	adultInput := strings.TrimSpace(strings.ToLower(scanner.Text()))
-	isAdult := false
-	if adultInput == "д" {
-		isAdult = true
+func addClient(scanner *bufio.Scanner, clients []Client) Client {
+	name := ""
+	for {
+		fmt.Print("Введите имя: ")
+		scanner.Scan()
+		if name = strings.TrimSpace(scanner.Text()); name == "" {
+			fmt.Println("Имя не может быть пустым")
+			continue
+		}
+		break
 	}
 
-	fmt.Print("Введите номер телефона: ")
-	scanner.Scan()
-	phone := strings.TrimSpace(scanner.Text())
+	isAdult := false
+	for {
+		fmt.Print("Клиент совершеннолетний? Д/Н: ")
+		scanner.Scan()
+		adultInput := strings.TrimSpace(strings.ToLower(scanner.Text()))
+		if adultInput == "д" {
+			isAdult = true
+			break
+		}
+		if adultInput == "н" {
+			break
+		}
+		fmt.Println("Некорректный ввод")
+
+	}
+
+	phone := ""
+	for {
+		fmt.Print("Введите номер телефона: ")
+		scanner.Scan()
+		if phone = strings.TrimSpace(scanner.Text()); phone == "" {
+			fmt.Println("Номер телефона не может быть пустым")
+			continue
+		} else {
+			phoneExists := false
+			for _, v := range clients {
+				if v.Phone == phone {
+					fmt.Printf("Номер %s телефона принадлежит клиенту: %s, с ID: %s\n",
+						phone,
+						v.Name,
+						v.ID)
+					phoneExists = true
+					break
+				}
+			}
+			if phoneExists {
+				continue
+			}
+		}
+		break
+	}
 
 	fmt.Print("Введите соц. сеть: ")
 	scanner.Scan()
 	social := strings.TrimSpace(scanner.Text())
 
 	client := Client{
-		ID:      id,
+		ID:      uuid.NewString(),
 		Name:    name,
 		IsAdult: isAdult,
 		Phone:   phone,
@@ -149,17 +186,9 @@ func findClient(scanner *bufio.Scanner, clients []Client) {
 	fmt.Print("Введите ID клиента: ")
 	scanner.Scan()
 
-	input := strings.TrimSpace(scanner.Text())
-
 	fmt.Println("-------------------")
 
-	id, err := strconv.Atoi(input)
-	if err != nil {
-		fmt.Println("ID должен быть числом")
-		return
-	}
-
-	client, err := findClientByID(clients, id)
+	client, err := findClientByID(clients, strings.TrimSpace(scanner.Text()))
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("-------------------")
@@ -173,17 +202,9 @@ func changeClient(scanner *bufio.Scanner, clients []Client) {
 	fmt.Print("Введите ID клиента для изменения: ")
 	scanner.Scan()
 
-	input := strings.TrimSpace(scanner.Text())
-
 	fmt.Println("-------------------")
 
-	id, err := strconv.Atoi(input)
-	if err != nil {
-		fmt.Println("ID должен быть числом")
-		return
-	}
-
-	index, err := findClientIndexByID(clients, id)
+	index, err := findClientIndexByID(clients, strings.TrimSpace(scanner.Text()))
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("-------------------")
@@ -204,28 +225,69 @@ func changeClient(scanner *bufio.Scanner, clients []Client) {
 
 		switch infoToChange {
 		case "1":
-			fmt.Println("Введите новое имя")
-			scanner.Scan()
-			newName := strings.TrimSpace(scanner.Text())
-			clients[index].Name = newName
+			for {
+				fmt.Println("Введите новое имя или 0 для отмены")
+				scanner.Scan()
+				newName := strings.TrimSpace(scanner.Text())
+				if newName == "0" {
+					break
+				}
+				if newName == "" {
+					fmt.Println("Имя не может быть пустым")
+					continue
+				}
+				clients[index].Name = newName
+				break
+			}
 		case "2":
-			fmt.Println("Введите новый номер")
-			scanner.Scan()
-			newPhone := strings.TrimSpace(scanner.Text())
-			clients[index].Phone = newPhone
+			for {
+				fmt.Println("Введите новый номер или 0 для отмены")
+				scanner.Scan()
+				newPhone := strings.TrimSpace(scanner.Text())
+				if newPhone == "0" {
+					break
+				}
+				if newPhone == "" {
+					fmt.Println("Номер телефона не может быть пустым")
+					continue
+				} else {
+					phoneExists := false
+					for _, v := range clients {
+						if v.Phone == newPhone && v.ID != clients[index].ID {
+							fmt.Printf("Номер %s телефона принадлежит клиенту: %s, с ID: %s\n",
+								newPhone,
+								v.Name,
+								v.ID)
+							phoneExists = true
+							break
+						}
+					}
+					if phoneExists {
+						continue
+					}
+				}
+				clients[index].Phone = newPhone
+				break
+			}
 		case "3":
 			fmt.Println("Введите новую соц. сеть")
 			scanner.Scan()
 			newSocial := strings.TrimSpace(scanner.Text())
 			clients[index].Social = newSocial
 		case "4":
-			fmt.Print("Клиент совершеннолетний? Д/Н: ")
-			scanner.Scan()
-			newIsAdult := strings.ToLower(strings.TrimSpace(scanner.Text()))
-			if newIsAdult == "д" {
-				clients[index].IsAdult = true
-			} else {
-				clients[index].IsAdult = false
+			for {
+				fmt.Print("Клиент совершеннолетний? Д/Н: ")
+				scanner.Scan()
+				newIsAdult := strings.ToLower(strings.TrimSpace(scanner.Text()))
+				if newIsAdult == "д" {
+					clients[index].IsAdult = true
+					break
+				}
+				if newIsAdult == "н" {
+					clients[index].IsAdult = false
+					break
+				}
+				fmt.Println("Некорректный ввод")
 			}
 		case "0":
 			printClient(clients[index])
@@ -239,20 +301,15 @@ func changeClient(scanner *bufio.Scanner, clients []Client) {
 
 func deleteClientChoice(scanner *bufio.Scanner, clients []Client) []Client {
 	for {
+		fmt.Println("Для выхода - 0")
 		fmt.Print("Введите ID клиента для удаления: ")
 		scanner.Scan()
-
-		input := strings.TrimSpace(scanner.Text())
-
+		if strings.TrimSpace(scanner.Text()) == "0" {
+			return clients
+		}
 		fmt.Println("-------------------")
 
-		id, err := strconv.Atoi(input)
-		if err != nil {
-			fmt.Println("ID должен быть числом")
-			continue
-		}
-
-		index, err := findClientIndexByID(clients, id)
+		index, err := findClientIndexByID(clients, strings.TrimSpace(scanner.Text()))
 		if err != nil {
 			fmt.Println(err)
 			fmt.Println("-------------------")
@@ -266,16 +323,16 @@ func deleteClientChoice(scanner *bufio.Scanner, clients []Client) []Client {
 			fmt.Print("Выберите действие: ")
 
 			scanner.Scan()
-			scanerDelete := strings.TrimSpace(strings.ToLower(scanner.Text()))
+			deleteChoice := strings.TrimSpace(strings.ToLower(scanner.Text()))
 			fmt.Println("-------------------")
 
-			switch scanerDelete {
+			switch deleteChoice {
 			case "1":
 				fmt.Println("Вы уверены?")
 				fmt.Println("1 — Да")
 				fmt.Println("0 — Нет")
 				scanner.Scan()
-				if strings.TrimSpace(strings.ToLower(scanner.Text())) == "1" {
+				if strings.TrimSpace(scanner.Text()) == "1" {
 					clients = slices.Delete(clients, index, index+1)
 					fmt.Println("Клиент удалён")
 					return clients
